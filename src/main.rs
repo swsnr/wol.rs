06ed138c0982f28cb35656a50c0e61652c6c5094
Sebastian Wiesner <sebastian@swsnr.de>
2025-03-24 16:28:25 +0100

    Tighten clippy lints and warnings for main

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,32 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// #![deny(warnings, clippy::all, clippy::pedantic,
-//     // Do cfg(test) right
-//     clippy::cfg_not_test,
-//     clippy::tests_outside_test_module,
-//     // Guard against left-over debugging output
-//     clippy::dbg_macro,
-//     clippy::unimplemented,
-//     clippy::use_debug,
-//     clippy::todo,
-//     // Require correct safety docs
-//     clippy::undocumented_unsafe_blocks,
-//     clippy::unnecessary_safety_comment,
-//     clippy::unnecessary_safety_doc,
-//     // We must use Gtk's APIs to exit the app.
-//     clippy::exit,
-//     // Don't panic carelessly
-//     clippy::get_unwrap,
-//     clippy::unused_result_ok,
-//     clippy::unwrap_in_result,
-//     clippy::indexing_slicing,
-//     // Do not carelessly ignore errors
-//     clippy::let_underscore_must_use,
-//     clippy::let_underscore_untyped,
-//     // Code smells
-//     clippy::float_cmp_const,
-//     clippy::string_to_string,
-//     clippy::if_then_some_else_none,
-//     clippy::large_include_file,
-//     // Disable as casts
-//     clippy::as_conversions,
-// )]
+#![deny(warnings, clippy::all, clippy::pedantic,
+    // Do cfg(test) right
+    clippy::cfg_not_test,
+    clippy::tests_outside_test_module,
+    // Guard against left-over debugging output
+    clippy::dbg_macro,
+    clippy::unimplemented,
+    clippy::use_debug,
+    clippy::todo,
+    // Don't panic carelessly
+    clippy::get_unwrap,
+    clippy::unused_result_ok,
+    clippy::unwrap_in_result,
+    clippy::indexing_slicing,
+    // Do not carelessly ignore errors
+    clippy::let_underscore_must_use,
+    clippy::let_underscore_untyped,
+    // Code smells
+    clippy::float_cmp_const,
+    clippy::string_to_string,
+    clippy::if_then_some_else_none,
+    clippy::large_include_file,
+    // Disable as casts
+    clippy::as_conversions,
+)]
+#![forbid(unsafe_code)]
 
 use std::{
     fmt::Display,
@@ -82,7 +77,7 @@ impl WakeUpTarget {
                 let mut socket_addrs = (dns.as_str(), self.port).to_socket_addrs()?;
                 let socket_addr = match mode {
                     ResolveMode::Default => socket_addrs.next(),
-                    ResolveMode::PreferIpv6 => socket_addrs.find(|addr| addr.is_ipv6()),
+                    ResolveMode::PreferIpv6 => socket_addrs.find(SocketAddr::is_ipv6),
                 };
                 if let Some(socket_addr) = socket_addr {
                     Ok(ResolvedWakeUpTarget {
@@ -179,6 +174,7 @@ struct CliArgs {
     ///
     /// If the password is omitted, prompt for the password.
     #[arg(long = "passwd")]
+    #[allow(clippy::option_option)]
     passwd: Option<Option<String>>,
     /// Hardware addresses to wake up.
     #[arg(value_name = "MAC-ADDRESS", required_unless_present("file"))]
@@ -212,7 +208,7 @@ fn wakeup(target: &WakeUpTarget, mode: ResolveMode, verbose: bool) -> std::io::R
             target.hardware_address, target.host, target.port
         );
     } else {
-        println!("Waking up {}...", target.hardware_address)
+        println!("Waking up {}...", target.hardware_address);
     }
     let target = target.resolve(mode)?;
     let bind_address = if target.socket_addr.is_ipv6() {
