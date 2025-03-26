cb90b651746d1880a24b0f11e15fb00948dba18b
Sebastian Wiesner <sebastian@swsnr.de>
2025-03-26 19:11:18 +0100

    Add copyright and project URL to --help

--- src/main.rs
+++ src/main.rs
@@ -146,8 +146,10 @@ impl FromStr for SecureOn {
     }
 }
 
-const AFTER_HELP: &str =
-    "This program is subject to the terms of the Mozilla Public License, v. 2.0.
+const AFTER_HELP: &str = "Copyright (C) Sebastian Wiesner <sebastian@swsnr.de>
+https://github.com/swsnr/wol.rs
+
+This program is subject to the terms of the Mozilla Public License, v. 2.0.
 If a copy of the MPL was not distributed with this file,
 You can obtain one at http://mozilla.org/MPL/2.0/.";
 
